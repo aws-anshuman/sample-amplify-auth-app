ffbd7967e5cfc0f02e3cdfdad9e53eae81161a03
Anshuman Mishra <manshum@amazon.com>
2022-07-06 15:56:05 +1000

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://r4a45i9ej6.execute-api.us-east-1.amazonaws.com/dev",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_UMfXDn84G",
+    APP_CLIENT_ID: "1qn3r7miu4d270bbvisc1l8uke",
+    IDENTITY_POOL_ID: "us-east-1:96ff844f-964c-4486-a323-1d8ef46a0dd1",
+  },
+};
